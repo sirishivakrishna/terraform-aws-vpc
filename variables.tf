aecaaa7a1c51ea9773c61485e45e7a03c35ddd78
shivakrishna <shivakrishnagodugu@outlook.com>
2023-09-30 08:56:20 +0530

    vpc module first commit

---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "cidr_block" {
+  
+}
+
+variable "enable_dns_hostnames" {
+  default = true
+}
+
+variable "enable_dns_support" {
+  default = true
+}
+
+variable "common_tags" {
+  default = {} #this means tag is optional
+  type = map
+}
+
+variable "vpc_tags" {
+  default = {} #this means tag is optional
+  type = map
+}
+
+variable "igw_tags" {
+  default = {} #this means tag is optional
+  type = map
+}
+variable "public_subnet_cidr" {
+    
+}
+variable "public_subnet_names" {
+    
+}
+variable "azs" {
+    
+}
+variable "private_subnet_cidr" {
+    
+}
+variable "private_subnet_names" {
+    
+}
+
+variable "database_subnet_cidr" {
+    
+}
+variable "database_subnet_names" {
+    
+}
